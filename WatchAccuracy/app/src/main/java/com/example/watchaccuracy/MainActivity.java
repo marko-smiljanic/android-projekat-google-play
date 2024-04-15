b15ac0c7d27a75b38bf5ef17f2a4e2cdd1773ad2
marko-smiljanic <75691933+marko-smiljanic@users.noreply.github.com>
2024-04-15 09:01:12 +0200

    .

--- WatchAccuracy/app/src/main/java/com/example/watchaccuracy/MainActivity.java
+++ WatchAccuracy/app/src/main/java/com/example/watchaccuracy/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         //dodati proveru dozvola
         //da li ce biti u redu prikaz za tablet?
         //proveriti gde su stare baze
+        //git test
+        //sf.dsjadhp
 
 
     }
